f239d4cdfe7487311e65ec6b13fb3bf0f0326b50
Eugene Krasotkin <still-neon@tut.by>
2017-04-05 17:16:09 +0300

    2

---
+++ src/test/java/LogIn.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import pageobjects.MailRuLoginPage;
+import pageobjects.MailRuMailPage;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testng.Assert.assertEquals;
+
+public class LogIn {
+    private static final String ENTER = "Вход";
+    private static final String EXIT = "выход";
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void login() throws InterruptedException {
+        System.out.print("");
+        MailRuLoginPage mailRuLoginPage = new MailRuLoginPage(driver);
+        mailRuLoginPage.load();
+        MailRuMailPage mailRuMailPage = mailRuLoginPage.login();
+        assertEquals(mailRuMailPage.getExitValue(), EXIT);
+    }
+
+    @Test
+    public void logout() throws InterruptedException {
+        MailRuLoginPage mailRuLoginPage = new MailRuLoginPage(driver);
+        mailRuLoginPage.load();
+        MailRuMailPage mailRuMailPage = mailRuLoginPage.login();
+        mailRuLoginPage = mailRuMailPage.logout();
+        assertEquals(mailRuLoginPage.getEnterValue(), ENTER);
+    }
+}
